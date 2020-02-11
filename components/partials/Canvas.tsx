96ff3358fbcf57fdbc2b7f1e104bef2330abc4d0
shuta13 <hollystarsun@gmail.com>
2020-02-11 23:39:30 +0900

    wip

--- components/partials/Canvas.tsx
+++ components/partials/Canvas.tsx
@@ -1,14 +1,34 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef, useCallback } from 'react'
 import {
   Scene,
   PerspectiveCamera,
-  WebGLRenderer
+  WebGLRenderer,
+  DirectionalLight
 } from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import RGBLoader from 'three/examples/jsm/loaders/RGBELoader'
+import { RoughnessMipmapper } from 'three/examples/jsm/utils/RoughnessMipmapper'
 
 import './Canvas.scss'
 
+const marcus_aurelius = require('../../public/marcus_aurelius/scene.gltf')
+console.log(marcus_aurelius)
+
+const image = require('../../public/lena_color.png')
+console.log(image)
+
 // ----------
 // types
+type AnimateParams = {
+  scene: Scene
+  camera: PerspectiveCamera
+  renderer: WebGLRenderer
+}
+type RendererParams = {
+  scene: Scene
+  camera: PerspectiveCamera
+  renderer: WebGLRenderer
+}
 type HandleResizeParams = {
   camera: PerspectiveCamera
   renderer: WebGLRenderer
@@ -26,15 +46,48 @@ const Canvas: React.FC = () => {
     // init scene
     const scene = new Scene()
     const camera = new PerspectiveCamera(75, width / height, 0.1, 1000)
+    camera.position.z = 2.0
     const renderer = new WebGLRenderer({ canvas: canvas, antialias: true })
     renderer.setClearColor('#1d1d1d')
     renderer.setSize(width, height)
-  
-    renderer.render(scene, camera)
+
+    const loader = new GLTFLoader()
+    let model;
+    loader.load('/', (gltf) => {
+      model = gltf.scene
+      model.name = 'marcus_aurelius'
+      model.scale.set(400.0, 400.0, 400.0)
+      model.position.set(0.0, -400.0, 0.0)
+      scene.add(model)
+    })
+    renderer.gammaFactor = 2.2
+
+    const light = new DirectionalLight(0xffffff)
+    light.intensity = 2
+    light.position.set(1, 1, 1,)
+    scene.add(light)
+
+    requestRef.current = window.requestAnimationFrame(() => animate({ scene, camera, renderer }))
 
     // resize
     window.addEventListener('resize', () => handleResize({ camera, renderer }))
   }
+
+  // animate
+  const requestRef = useRef(0)
+  const animate = useCallback(({ scene, camera, renderer }: AnimateParams) => {
+    render({ scene, camera, renderer })
+    requestRef.current = window.requestAnimationFrame(() => animate({ scene, camera, renderer }))
+  }, [])
+  useEffect(() => {
+    return () => window.cancelAnimationFrame(requestRef.current)
+  }, [animate])
+
+  // render
+  const render = ({ scene, camera, renderer }: RendererParams) => {
+    renderer.render(scene, camera)
+  }
+
   // handle resize
   const handleResize = ({ camera, renderer }: HandleResizeParams) => {
     const width = window.innerWidth
