747b49a212f37225bd984f3c0cdc67de5c21d829
shuta13 <hollystarsun@gmail.com>
2020-02-11 22:16:52 +0900

    chore: setup images

--- components/partials/Canvas.tsx
+++ components/partials/Canvas.tsx
@@ -5,6 +5,9 @@ import './Canvas.scss'
 const fragment = require('../shaders/Canvas/frag.glsl')
 console.log(fragment.default)
 
+const lena = require('../../public/static/lena_color.png')
+console.log(lena)
+
 const Canvas: React.FC = () => {
   return (
     <div className="CanvasWrap">
