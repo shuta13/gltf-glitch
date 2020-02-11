556cb6a20090f1c71128aac156b4e861dee9ad30
shuta13 <hollystarsun@gmail.com>
2020-02-11 22:29:27 +0900

    chore: setup three

--- components/partials/Canvas.tsx
+++ components/partials/Canvas.tsx
@@ -1,20 +1,52 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import {
+  Scene,
+  PerspectiveCamera,
+  WebGLRenderer
+} from 'three'
 
 import './Canvas.scss'
 
-const fragment = require('../shaders/Canvas/frag.glsl')
-console.log(fragment.default)
-
-const lena = require('../../public/static/lena_color.png')
-console.log(lena)
-
-const marcus_aurelius = require('../../public/static/gltf/marcus_aurelius/scene.gltf')
-console.log(marcus_aurelius)
+// ----------
+// types
+type HandleResizeParams = {
+  camera: PerspectiveCamera
+  renderer: WebGLRenderer
+}
+// ----------
 
 const Canvas: React.FC = () => {
+  const width = window.innerWidth
+  const height = window.innerHeight
+  const onCanvasLoaded = (canvas: HTMLCanvasElement) => {
+    if (!canvas) {
+      return
+    }
+  
+    // init scene
+    const scene = new Scene()
+    const camera = new PerspectiveCamera(75, width / height, 0.1, 1000)
+    const renderer = new WebGLRenderer({ canvas: canvas, antialias: true })
+    renderer.setClearColor('#1d1d1d')
+    renderer.setSize(width, height)
+  
+    renderer.render(scene, camera)
+
+    // resize
+    window.addEventListener('resize', () => handleResize({ camera, renderer }))
+  }
+  // handle resize
+  const handleResize = ({ camera, renderer }: HandleResizeParams) => {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+  }
+  useEffect(() => {
+    return () => window.removeEventListener('resize', () => handleResize)
+  })
   return (
     <div className="CanvasWrap">
-      this is canvas
+      <canvas ref={onCanvasLoaded} />
     </div>
   )
 }
